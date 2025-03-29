a45c9330a3803d941a31de1a5e78798251056de4
OshadaX <navindraoshada@gmail.com>
2025-03-29 16:29:27 +0530

    Set up Spring Boot backend with user registration API and React frontend

---
+++ backend/backend/src/main/java/com/skillsharing/backend/SkillSharingPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skillsharing.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSharingPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSharingPlatformBackendApplication.class, args);
+	}
+
+}
